8fbd44f24b380a7006da1445954a0e7b68fed6ff
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-02-15 22:24:13 -0300

    advent of code day 1 part 2 solved

--- 1/day1.js
+++ 1/day1.js
@@ -21,7 +21,17 @@ function spelledNumToNum (arrayOfStr) {
         'six': '6',
         'seven': '7',
         'eight': '8',
-        'nine': '9'
+        'nine': '9',
+
+        'eno': '1',
+        'owt': '2',
+        'eerht': '3',
+        'ruof': '4',
+        'evif': '5',
+        'xis': '6',
+        'neves': '7',
+        'thgie': '8',
+        'enin': '9'
     }
 
     arrayOfStr.forEach((str, index) => {
@@ -41,37 +51,29 @@ function trebuchet(input) {
     let inputStrNums = [];
     // regex for detecting integers (\d) or spelled out nums, 
     // also with flag g (global search so it doesn't returns just the 1st) and i (case insensitive)
-    let regex = /\d|(one|two|three|four|five|six|seven|eight|nine)/gi
-
+    let regex = /\d|(one|two|three|four|five|six|seven|eight|nine)|(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/ig;
+    
     for (let i = 0; i < input.length; i++) {
         // let inputStrArray = input[i].split(''); // deprecated for part 2
         let nums = [];
+        let reversedNums = [];
         let inputCalibrationValue = ''; // calibration value for each string
 
         // inputStrArray.forEach((char) => !isNaN(char) ? nums.push(char) : ''); // deprecated for part 2
         nums = input[i].match(regex);
+        reversedNums = input[i].split('').reverse().join('').match(regex);
         
         // convert spelled out nums to str nums
         nums = spelledNumToNum(nums);
-        
-        console.log(`nums array for iteration ${i}: ${nums}`)
+        reversedNums = spelledNumToNum(reversedNums);
 
-        if (nums.length > 2) {
-            // retrieve first and last index
-            inputCalibrationValue = nums[0] + nums[nums.length - 1];
-        } else if (nums.length === 1) {
-            // resulting num must be 2 digits, if theres only one it's the same digit
-            inputCalibrationValue = nums[0] + nums[0];
-        } else {
-            // nums.length === 0 || 2; concatenate all and push to inputStrNums
-            nums.forEach(num => inputCalibrationValue += num);
-        }
+        inputCalibrationValue += nums[0] + reversedNums[0]
+
         console.log(`calibration value for i=${i} -> ${inputCalibrationValue}`)
         inputStrNums.push(inputCalibrationValue);
     }
 
     totalSum = inputStrNums.reduce((accumulator, current) => {
-        console.log(`accumulator = ${accumulator} / current = ${current}`)
         return accumulator + +current
     }, 0)
 
